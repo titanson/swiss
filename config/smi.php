c066c13c24c44bc4d7dd2cbb580561f101b631cb
tabuna <bliz48rus@gmail.com>
2020-10-14 19:30:34 +0300

    Added source

--- config/smi.php
+++ config/smi.php
@@ -67,23 +67,31 @@ return [
         'https://www.srf.ch/news/bnf/rss/1646',
         'https://www.nzz.ch/recent.rss',
         'https://www.blick.ch/news/rss.xml',
-
         'https://partner-feeds.publishing.tamedia.ch/rss/tagesanzeiger/ticker',
         'https://bazonline.ch/basel/rss.html',
         'https://partner-feeds.publishing.tamedia.ch/rss/derbund/ticker',
         'https://partner-feeds.20min.ch/rss/20minuten',
         'https://partner-feeds.publishing.tamedia.ch/rss/bazonline/ticker',
-
         'https://www.faz.net/rss/aktuell/',
         'https://www.srf.ch/news/bnf/rss/1890',
         'https://telebasel.ch/feed/',
         'https://www.watson.ch/api/1.0/rss/index.xml?tag=Schweiz',
-        
-        /*
-        'https://www.independent.co.uk/topic/switzerland/rss',
-        'https://feeds.thelocal.com/rss/ch',
-        */
-
+        'https://newsfeed.zeit.de/index',
+        'https://rss.sueddeutsche.de/app/service/rss/alles/index.rss?output=rss',
+        'https://partner-feeds.publishing.tamedia.ch/rss/bernerzeitung/ticker',
+        'https://www.luzernerzeitung.ch/schweiz.rss',
+        'https://www.tagblatt.ch/schweiz.rss',
+        'https://www.persoenlich.com/rss/news.xml',
+        'https://insideparadeplatz.ch/feed/',
+        'http://www.news.ch/rss/letztenews.xml',
+        'https://www.netzwoche.ch/taxonomy/term/31/feed',
+        'https://t3n.de/rss.xml',
+        'https://rss.golem.de/rss.php?feed=RSS2.0',
+        'www.heise.de/rss/heise-atom.xml',
+        'https://www.derstandard.at/rss',
+        'https://www.diepresse.com/rss/home',
+        'https://www.kleinezeitung.at/rss/chronik',
+        'https://kurier.at/xml/rssd',
     ],
 
 
