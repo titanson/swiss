954e7348c7c8d97048a49e53a4ecf762560e3cb4
tabuna <bliz48rus@gmail.com>
2020-10-14 16:37:20 +0300

    Added source

--- config/smi.php
+++ config/smi.php
@@ -67,11 +67,23 @@ return [
         'https://www.srf.ch/news/bnf/rss/1646',
         'https://www.nzz.ch/recent.rss',
         'https://www.blick.ch/news/rss.xml',
+
         'https://partner-feeds.publishing.tamedia.ch/rss/tagesanzeiger/ticker',
         'https://bazonline.ch/basel/rss.html',
         'https://partner-feeds.publishing.tamedia.ch/rss/derbund/ticker',
         'https://partner-feeds.20min.ch/rss/20minuten',
         'https://partner-feeds.publishing.tamedia.ch/rss/bazonline/ticker',
+
+        'https://www.faz.net/rss/aktuell/',
+        'https://www.srf.ch/news/bnf/rss/1890',
+        'https://telebasel.ch/feed/',
+        'https://www.watson.ch/api/1.0/rss/index.xml?tag=Schweiz',
+        
+        /*
+        'https://www.independent.co.uk/topic/switzerland/rss',
+        'https://feeds.thelocal.com/rss/ch',
+        */
+
     ],
 
 
